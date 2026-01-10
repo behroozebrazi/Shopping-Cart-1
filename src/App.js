3f9c17b4d3ff7a8c333d63691cb25e02dd7d56b5
behroozebrazi <behber@berkeley.edu>
2026-01-10 14:55:15 -0800

    initial folders, index, and app files

--- src/App.js
+++ src/App.js
@@ -1,2 +1,16 @@
+import React from 'react';
 import './App.css';
 
+class App extends React.Component {
+
+
+  render() {
+    return (
+      <div className='App'>
+        Start Here
+      </div>
+    )
+  }
+}
+
+export default App
